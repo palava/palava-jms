144decbb2f5190bd6ef54d398a04ce8fb836fe77
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-05-26 18:01:51 +0000

    renamed AbstractRebindingModule to AbstractRebindModule

--- src/main/java/de/cosmocode/palava/jms/JmsModule.java
+++ src/main/java/de/cosmocode/palava/jms/JmsModule.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.palava.jms;
 
 import java.lang.annotation.Annotation;
@@ -18,7 +34,7 @@ import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 
-import de.cosmocode.palava.core.inject.AbstractRebindingModule;
+import de.cosmocode.palava.core.inject.AbstractRebindModule;
 import de.cosmocode.palava.core.inject.Config;
 import de.cosmocode.palava.core.inject.RebindModule;
 
@@ -29,7 +45,7 @@ import de.cosmocode.palava.core.inject.RebindModule;
  * @since 1.0
  * @author Willi Schoenborn
  */
-public final class JmsModule extends AbstractRebindingModule {
+public final class JmsModule extends AbstractRebindModule {
 
     private final Class<? extends Annotation> annotation;
     
