b085cabbd5661f9f4826e3ed66e9b4a5e41aeaff
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-08-16 17:02:52 +0000

    added JmsConfig to provide common prefix

---
+++ src/main/java/de/cosmocode/palava/jms/JmsConfig.java
@@ -0,0 +1,17 @@
+package de.cosmocode.palava.jms;
+
+/**
+ * Static constant holder class for jms config key names.
+ *
+ * @since 1.1
+ * @author Willi Schoenborn
+ */
+public final class JmsConfig {
+
+    public static final String PREFIX = "jms.";
+    
+    private JmsConfig() {
+        
+    }
+
+}
